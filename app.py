e9848349431d9363784e44bc2c72323b1fcc19ff
jinrumiao <122008339+jinrumiao@users.noreply.github.com>
2023-08-07 12:04:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -42,14 +42,12 @@ def handle_message(event):
         }
     ]
 
-    welcome_message = TextSendMessage(text='''
-    $ Agave Finance $
+    welcome_message = TextSendMessage(text='''$ Agave Finance $
     您好，歡迎加入成為 Agave Finance 的好友!!!
     我是Agave財經小幫手~
     下方選單有：
     股票查詢、油價查詢、匯率查詢、自動提醒、資訊整理、使用說明
-    使用上有任何問題可以參考使用說明
-    ''', emojis=emojis)
+    使用上有任何問題可以參考使用說明''', emojis=emojis)
 
     sticker_message = StickerSendMessage(
         package_id="11537", sticker_id="52002735"
