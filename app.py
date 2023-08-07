ff8ec2f0bb2f9278e2195213d81e43cf4cc1d777
jinrumiao <122008339+jinrumiao@users.noreply.github.com>
2023-08-07 16:08:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -23,10 +23,20 @@ def callback():
 def handle_message(event):
     message_text = str(event.message.text).lower()
 
+    # ############"使用說明"############
     if message_text == "@使用說明":
         about_us_event(event)
         Usage(event)
 
+    # ############"油價查詢"############
+    if message_text == "油價查詢":
+        content = oil_price()
+        line_bot_api.reply_message(
+        event.reply_token, 
+        TextSendMessage(content)
+        )
+
+    # ############"@小幫手"############
     if message_text == "@小幫手":
         button_template = Template_msg()
         line_bot_api.reply_message(
